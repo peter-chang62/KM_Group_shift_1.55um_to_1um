a8f0a85f8703b70df6e9f955b6614905871e0cd1
Peter <pech6593@colorado.edu>
2022-06-13 15:53:45 -0600

    update

--- script.py
+++ script.py
@@ -1,13 +1,8 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pynlo_peter.Fiber_PPLN_NLSE as fpn
+import matplotlib.pyplot as plt
+import simulationHeader as sh
 
-
-def normalize(vec):
-    return vec / np.max(abs(vec))
-
-
-# %%____________________________________________________________________________________________________________________
 intensity = np.genfromtxt("KM_FROG_RETRIEVAL/1p68nJ_30p2cmpatch_Intensity.csv", delimiter=',')
 spectrum = np.genfromtxt("KM_FROG_RETRIEVAL/1p68nJ_30p2cmpatch_Spectrum.csv", delimiter=',')
 
@@ -19,14 +14,18 @@ pulse.set_AW_experiment(spectrum[:, 0] * 1e-3, amp)
 
 # %%____________________________________________________________________________________________________________________
 # checks out!
-plt.figure()
-plt.plot(pulse.wl_um * 1e3, normalize(pulse.AW.__abs__() ** 2))
-plt.plot(spectrum[:, 0], normalize(spectrum[:, 1]))
-plt.xlim(1400, 1800)
-
-plt.figure()
-plt.plot(pulse.T_ps, normalize(pulse.AT.__abs__() ** 2))
-plt.plot(intensity[:, 0] * 1e12, normalize(intensity[:, 1]))
+# plt.figure()
+# plt.plot(pulse.wl_um * 1e3, sh.normalize(pulse.AW.__abs__() ** 2))
+# plt.plot(spectrum[:, 0], sh.normalize(spectrum[:, 1]))
+# plt.xlim(1400, 1800)
+#
+# plt.figure()
+# plt.plot(pulse.T_ps, sh.normalize(pulse.AT.__abs__() ** 2))
+# plt.plot(intensity[:, 0] * 1e12, sh.normalize(intensity[:, 1]))
 
 # %%____________________________________________________________________________________________________________________
-pulse.set_epp(1.68e-9)
+sim = sh.simulate(pulse=pulse,
+                  fiber=sh.fiber_pm1550,
+                  length_cm=11,
+                  epp_nJ=1.68,
+                  nsteps=200)
